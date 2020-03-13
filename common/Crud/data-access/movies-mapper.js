7be315b5070f6b5e22ad3c7275912e498324897e
nitish.gupta <nitish.gupta@newgen.co.in>
2020-03-13 18:37:52 +0530

    Pushed docker-compose with redis changes.

--- common/Crud/data-access/movies-mapper.js
+++ common/Crud/data-access/movies-mapper.js
@@ -1,17 +1,36 @@
 
 var db = require('../db')
+var redis = require('redis');
+var redisclient = redis.createClient("6379", "192.168.60.45");
+
+
+// var redis = require('node-redis')
+// var redisclient = redis.createClient("6379", "192.168.60.45")
 
 exports.get = function(req,res)
 {
-   var sql = "select * from movies";
-   db.query(sql,function(err,rows,fields)
-   {
-       if(err)
-       {
-           res.status(500).send(({error:'something failed'}));
-       }
-       res.json(rows);
-   }) 
+    redisclient.get('getMoive', function(error, result) {
+        if (error) throw error;
+        if(result!=null && result!=undefined)
+        {
+            console.log('GET result ->', result.length);
+            res.json(result);
+        }
+        else
+        {
+            var sql = "select * from movies";
+            db.query(sql,function(err,rows,fields)
+            {
+                if(err)
+                {
+                    res.status(500).send(({error:'something failed'}));
+                }
+                redisclient.set('getMoive',JSON.stringify(rows));
+                res.json(rows);
+            }) 
+        }
+        
+      });
 };
 
 exports.create = function(req,res)
